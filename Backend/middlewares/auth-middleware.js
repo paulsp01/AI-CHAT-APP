b61ef5d3f8a1c097597199b5f378d54c23500a41
swarnalee paul <paulswarnalee01@gmail.com>
2025-02-13 21:14:30 +0530

    socket io implementation done

--- Backend/middlewares/auth-middleware.js
+++ Backend/middlewares/auth-middleware.js
@@ -11,7 +11,7 @@ export const authMiddleware=async (req ,res , next)=>{
     try {
         
         const token=req.cookies.token || req.headers.authorization.split(' ')[1];
-        console.log("token",token);
+       
         if(!token){
             return res.status(401).send({error:"Unauthorized User"})
         }
