74c11373b36eb79bac5b4441b315ba3f7557c75b
swarnalee paul <paulswarnalee01@gmail.com>
2025-02-15 20:24:50 +0530

    commit

--- Backend/models/project.model.js
+++ Backend/models/project.model.js
@@ -4,7 +4,7 @@ import mongoose from 'mongoose';
 const projectSchema = new mongoose.Schema({
     name: {
         type: String,
-        lowercase: true,
+        //lowercase: true,
         required: true,
         trim: true,
         unique: [ true, 'Project name must be unique' ],
