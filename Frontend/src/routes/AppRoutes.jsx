8d36ae1db5fbca9794891da5f2c43a40109d8abd
swarnalee paul <paulswarnalee01@gmail.com>
2025-02-15 21:02:56 +0530

    commit

--- Frontend/src/routes/AppRoutes.jsx
+++ Frontend/src/routes/AppRoutes.jsx
@@ -5,7 +5,7 @@ import Register from '../screens/Register'
 import Home from '../screens/Home'
 import Project from '../screens/Project'
 import UserAuth from '../auth/UserAuth'
-import login from "../screens/login"
+import Login from "../screens/login"
 
 
 const AppRoutes = () => {
@@ -14,7 +14,7 @@ const AppRoutes = () => {
         <BrowserRouter>
        <Routes>
         <Route path="/" element={<UserAuth><Home /></UserAuth>} />
-        <Route path="/login" element={<login/>} />
+        <Route path="/login" element={<Login/>} />
         <Route path="/register" element={<Register/>} />
         <Route path="/project" element={<UserAuth><Project/></UserAuth>} />
        
