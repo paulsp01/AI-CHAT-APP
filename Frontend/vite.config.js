6ef3eeee577ff7edeb1788c400b55f2e6d3df752
swarnalee paul <paulswarnalee01@gmail.com>
2025-02-15 23:50:50 +0530

    commit

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -4,11 +4,18 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+
+  build: {
+    chunkSizeWarningLimit: 2000, 
+  },
+  
   server: {
     headers: {
       "Cross-Origin-Embedder-Policy": "require-corp",
       "Cross-Origin-Opener-Policy": "same-origin"
     },
+    host: '0.0.0.0', // Bind to all network interfaces
+    port: 5173, // Use Render's PORT or default to 5173
     
   }
 })
